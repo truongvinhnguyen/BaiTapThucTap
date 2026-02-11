c02d569e270cfba782e67c0b09736d10892c88e7
truongvinhnguyen <125782088+truongvinhnguyen@users.noreply.github.com>
2026-02-11 14:55:19 +0700

    add spring boot project

---
+++ src/main/java/org/example/baitapthuctap/BaiTapThucTapApplication.java
@@ -0,0 +1,13 @@
+package org.example.baitapthuctap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaiTapThucTapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaiTapThucTapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/baitapthuctap/BaiTapThucTapApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.baitapthuctap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaiTapThucTapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
